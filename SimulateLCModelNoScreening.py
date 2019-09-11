eecb23caa2d12f7a0e825f8bf7a69b5ca9ddc608
PHONG NGUYEN <vietphong (dot) nguyen (at) gmail (dot) com>
2019-09-11 08:27:10 -0400

    bug fixed in Displaying the current age and life remain

--- SimulateLCModelNoScreening.py
+++ SimulateLCModelNoScreening.py
@@ -12,14 +12,27 @@ class SimulateLCModelNoScreening:
         # LC_result = [person, years_remain]
         # years_remain = [year remain, disease_free, local_LC, regional_LC, distant_LC, death_other_causes]
         # local_LC = [number of local_LC, month infected]
-        person = LC_result[0]
-        years_remain_result = LC_result[1]
-        years_remain = years_remain_result[0]
-        disease_free = years_remain_result[1]
-        local_LC = years_remain_result[2]
-        regional_LC = years_remain_result[3]
-        distant_LC = years_remain_result[4]
-        death_other_causes = years_remain_result[5]
+        try:
+            person = LC_result[0]
+            age = person.age
+            years_remain_result = LC_result[1]
+            years_remain = years_remain_result[0]
+            disease_free = years_remain_result[1]
+            local_LC = years_remain_result[2]
+            regional_LC = years_remain_result[3]
+            distant_LC = years_remain_result[4]
+            death_other_causes = years_remain_result[5]
+            total_local_LC = str_sum(local_LC)
+            total_regional_LC = str_sum(regional_LC)
+            total_distant_LC = str_sum(distant_LC)
+        except TypeError:
+            age = 50
+            years_remain = 50
+            disease_free = 1
+            death_other_causes = 0
+            total_local_LC = "0"
+            total_regional_LC = "0"
+            total_distant_LC = "0"
 
         self.window = window
         HEIGHT = 800
@@ -49,17 +62,16 @@ class SimulateLCModelNoScreening:
         tk.Label(self.window, text=str(death_other_causes)[:5]).place(x=1010, y=440)
 
         # Display the Local LC
-        tk.Label(self.window, text=str_sum(local_LC)[:5]).place(x=225, y=295)
+        tk.Label(self.window, text=total_local_LC[:5]).place(x=225, y=295)
 
         # Display the Regional LC
-        tk.Label(self.window, text=str_sum(regional_LC)[:5]).place(x=225, y=397)
+        tk.Label(self.window, text=total_regional_LC[:5]).place(x=225, y=397)
 
         # Display the Distant LC
-        str_distant_LC = str_sum(distant_LC)[:5]
-        tk.Label(self.window, text=str_distant_LC).place(x=225, y=495)
+        tk.Label(self.window, text=total_distant_LC[:5]).place(x=225, y=495)
 
         # Display the Death LC
-        tk.Label(self.window, text=str_distant_LC).place(x=630, y=550)
+        tk.Label(self.window, text=total_distant_LC[:5]).place(x=630, y=550)
 
         # Display current age and life remain
         try:
@@ -69,20 +81,18 @@ class SimulateLCModelNoScreening:
             pointer_label2 = tk.Label(self.window, image=pointer_image)
         except tk.TclError:
             print("cant read the ./images/current age pointer copy.png")
-        x1 = int(1088 + (person.age - 50) * 9.505)
+        x1 = int(1088 + (age - 50) * 9.505)
         x2 = int(x1 + years_remain * 9.505)
-        # tk.Canvas.create_rectangle(x1, 383, x2, 383 + 5, outline="#fb0", fill="#fb0")   # Draw the life remain bar
         pointer_label.place(x=x1, y=383)
         pointer_label2.place(x=x2, y=383)
-        tk.Label(self.window, text=str(person.age) + " + " + str(years_remain)[:6])\
-            .place(x=x1-8, y=412)
+        tk.Label(self.window, text=str(age) + " + " + str(years_remain)[:6]).place(x=x1-8, y=412)
 
-
-
+        # Draw the life remain bar
+        canvas.create_rectangle(x1, 383, x2, 383 + 5, outline="red", fill="red")
 
         self.window.mainloop()
 
     def quit(self):
         self.window.destroy()
 
-# window_LC_model_no_screening = SimulateLCModelNoScreening()
+
